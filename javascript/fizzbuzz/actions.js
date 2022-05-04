e94f1fa3b7f4634a42099cdca2495bf2be3a1799
deepaksharmaongraph <deepaksharma.ongraph@gmail.com>
2022-05-04 19:19:07 +0530

    fizzbuzz demo in js

---
+++ javascript/fizzbuzz/actions.js
@@ -0,0 +1,24 @@
+class FizzBuzzActions {
+    print(msg) {
+
+    }
+
+    gt_100(i){
+
+    }
+
+    mod3_eq0(i){
+
+    }
+
+    mod5_eq0(i){
+
+    }
+
+    plus_1(i){
+
+    }
+
+}
+
+module.exports = FizzBuzzActions;
